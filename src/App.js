24f45b6989d72a0c816a199c33497e1921c57d4e
nemocominotto <r0797042@student.thomasmore.be>
2022-01-26 17:58:21 +0100

    login functie

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 import Navbar from "./components/Navbar";
-import {Router} from "react-router-dom";
 import Login from "./components/authentication/Login";
+
+import { useEffect } from 'react';
+
+import { observer } from 'mobx-react';
+import UserStore from './UserStore';
 
 import AdminIndex from './components/Administrator/AdminIndex';
 import AdminCreate from './components/Administrator/AdminCreate';
@@ -14,21 +18,37 @@ import TagEdit from './components/tags/TagEdit';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
+  useEffect(() => {
+    fetch('http://localhost:8050/administrators/login?email=' + sessionStorage.getItem('email') + '&password=' + sessionStorage.getItem('password'), {
+      method: "GET",
+      headers: {"Content-Type": "application/json"}
+    }).then(res => {
+      return res.json();
+    }).then(data => {
+      if (data) {
+        UserStore.isLoggedIn = true;
+        UserStore.email = sessionStorage.getItem('email');
+      }
+    })
+  }, []);
+  
+
   return (
     <div className="">
       <BrowserRouter>
         <Navbar></Navbar>
         <Switch>
-          <Route exact path='/administrators' component={AdminIndex} />
-          <Route exact path='/administrator/create' component={AdminCreate}/>
-          <Route exact path='/tags' component={TagIndex}/>
-          <Route exact path='/tag/create' component={TagCreate}/>
-          <Route path='/administrator/edit/:id' component={AdminEdit}/>
-          <Route path='/tag/edit/:id' component={TagEdit}/>
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/administrators' component={UserStore.isLoggedIn ? AdminIndex : ''} />
+          <Route exact path='/administrator/create' component={UserStore.isLoggedIn ? AdminCreate : ''}/>
+          <Route exact path='/tags' component={UserStore.isLoggedIn ? TagIndex : ''}/>
+          <Route exact path='/tag/create' component={UserStore.isLoggedIn ? TagCreate : ''}/>
+          <Route path='/administrator/edit/:id' component={UserStore.isLoggedIn ? AdminEdit : ''}/>
+          <Route path='/tag/edit/:id' component={UserStore.isLoggedIn ? TagEdit : ''}/>
         </Switch>
       </BrowserRouter>
     </div>
   );
 }
 
-export default App;
+export default observer(App);
