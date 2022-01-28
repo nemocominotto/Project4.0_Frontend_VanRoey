38979e34ae5667ca05dfeb1dfb7af6a5d49fe475
anthonydecap <r0805615@student.thomasmore.be>
2022-01-28 02:14:51 +0100

    tag verdeling post

--- src/components/Bezoekers/BezoekerCreate.js
+++ src/components/Bezoekers/BezoekerCreate.js
@@ -1,25 +1,71 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Api from '../../api/Api';
 import { useHistory,useParams } from 'react-router-dom';
+import { vi } from 'date-fns/locale';
 
 const BezoekerCreate = () => {
     const history = useHistory();
 
     const {id} = useParams();
 
+    const [visitortags, setVisitortags] = useState([]);
+    const [tags, setTags] = useState([]);
+
     const [visitID, setVisitID] = useState(id);
     const [name, setName] = useState('');
     const [lastname, setLastname] = useState('');
     const [email, setEmail] = useState('');
 
+    useEffect(() => {
+      Api.getAllTags().then(res => {
+        setTags(res.data);
+      }).then(() => {
+      
+      });
+
+      Api.getAllVisitorTags().then(res => {
+        setVisitortags(res.data);
+        console.log(res.data)
+      }).then(() => {
+          //console.log(res.data)
+          //setIsLoaded(true);
+      });
+    }, []);
+      
     const handleSubmit = (e) => {
         e.preventDefault();
         const visitor = {visitID, name, lastname, email}
+        let unusedTagId = 0;
+        let newvisitorId = 0;
 
-        Api.createVisitor(visitor).then(() => {
-          history.push(`/bezoek/edit/${visitor.visitID}`);
-        }); 
+        tags.forEach(tag => {
+          let found = false
+          visitortags.forEach(visitortag => {
+            if(tag.tagId == visitortag.tagId) {
+              found = true;
+            }
+          });
+
+          if(found == false)
+          {
+            unusedTagId = tag.tagId
+          }
+        });
+
+        console.log(unusedTagId);
+
+        Api.createVisitor(visitor).then(res => {
+          newvisitorId = res.data.visitorID
+        }).then(() => {
+          let visitorTag = {tagId:unusedTagId, visitorId:newvisitorId}
+          console.log(visitorTag);
+          Api.createVisitorTag(visitorTag).then(res => {
+            console.log(res.data);
+          }).then(() => {
+            history.push(`/bezoek/edit/${visitor.visitID}`);
+          });
+        });
     }
 
     return (
